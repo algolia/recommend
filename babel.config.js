2f462b9101fc58210cc8dfd9ea853f513d68045d
François Chalifour <francois.chalifour@gmail.com>
2021-05-06 16:30:23 +0200

    chore: update Babel config

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+module.exports = (api) => {
+  const isTest = api.env('test');
+  const modules = isTest ? 'commonjs' : false;
+
+  return {
+    presets: [
+      ['@babel/preset-react'],
+      [
+        '@babel/preset-env',
+        {
+          modules,
+        },
+      ],
+    ],
+  };
+};
