01acb06df0fd0e8b6e7626664fdd9f9a9170b2c6
shortcuts <vannicattec@gmail.com>
2021-05-18 14:18:17 +0200

    feat(ci): initialize circleCI

---
+++ ship.config.js
@@ -0,0 +1,64 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = {
+  monorepo: {
+    mainVersionFile: 'lerna.json',
+    // We rely on Lerna to bump our dependencies.
+    packagesToBump: [],
+    packagesToPublish: [
+      'packages/react-recommendations',
+      'packages/js-recommendations',
+    ],
+  },
+  publishCommand({ tag }) {
+    return `yarn publish --access public --tag ${tag}`;
+  },
+  versionUpdated({ exec, dir, version }) {
+    // Update package dependencies
+    exec(
+      `yarn lerna version ${version} --exact --no-git-tag-version --no-push --yes`
+    );
+
+    // Ship.js reads JSON and writes with `fs.writeFileSync(JSON.stringify(json, null, 2))`
+    // which causes a lint error in the `lerna.json` file.
+    exec('yarn eslint lerna.json --fix');
+
+    // Update version files
+    updatePackagesVersionFile({
+      version,
+      files: [
+        path.resolve(
+          dir,
+          'packages',
+          'react-recommendations',
+          'src',
+          'version.ts'
+        ),
+        path.resolve(
+          dir,
+          'packages',
+          'js-recommendations',
+          'src',
+          'version.ts'
+        ),
+      ],
+    });
+  },
+  // Skip preparation if it contains only `chore` commits
+  shouldPrepare({ releaseType, commitNumbersPerType }) {
+    const { fix = 0 } = commitNumbersPerType;
+
+    if (releaseType === 'patch' && fix === 0) {
+      return false;
+    }
+
+    return true;
+  },
+};
+
+function updatePackagesVersionFile({ version, files }) {
+  for (const file of files) {
+    fs.writeFileSync(file, `export const version = '${version}';\n`);
+  }
+}
