06ebd03078ada4804f552b9122026de2bfa0e63f
François Chalifour <francois.chalifour@gmail.com>
2021-06-28 11:34:48 +0200

    chore(recommend): add comment about duplicated logic

--- packages/recommend-js/src/useStatus.ts
+++ packages/recommend-js/src/useStatus.ts
@@ -1,6 +1,8 @@
 import { RecommendStatus } from '@algolia/recommend-vdom';
 import { useEffect, useRef, useState } from 'preact/hooks';
 
+// If you update this logic, please also update the implementation in the
+// `recommend-react` package.
 export function useStatus(initialStatus: RecommendStatus) {
   const timerId = useRef<ReturnType<typeof setTimeout> | undefined>(undefined);
   const [status, setStatus] = useState<RecommendStatus>(initialStatus);
--- packages/recommend-react/src/useStatus.ts
+++ packages/recommend-react/src/useStatus.ts
@@ -1,6 +1,8 @@
 import { RecommendStatus } from '@algolia/recommend-vdom';
 import { useEffect, useRef, useState } from 'react';
 
+// If you update this logic, please also update the implementation in the
+// `recommend-js` package.
 export function useStatus(initialStatus: RecommendStatus) {
   const timerId = useRef<ReturnType<typeof setTimeout> | undefined>(undefined);
   const [status, setStatus] = useState<RecommendStatus>(initialStatus);
