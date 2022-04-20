1880b15c94f266e5db6da304bb8be05f4404d2c7
Ana Maria Gabriela Lungu <heymarialungu@gmail.com>
2022-04-20 11:31:15 +0300

    fix: refactor

--- packages/recommend-core/src/getRelatedProducts.ts
+++ packages/recommend-core/src/getRelatedProducts.ts
@@ -34,8 +34,6 @@ export function getRelatedProducts<TObject>({
     .then((response) =>
       mapToRecommendations<ProductRecord<TObject>>({
         maxRecommendations,
-        // Multiple identical recommended `objectID`s can be returned b
-        // the engine, so we need to remove duplicates.
         hits: response.results.map((result) => result.hits),
         nrOfObjs: objectIDs.length,
       })
