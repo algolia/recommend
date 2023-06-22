b251714f539e5cc3732c8ab0b2075d21ce655bb3
Raed <raed.chammam@algolia.com>
2023-06-22 16:07:40 +0200

    remove coverage

--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,4 @@ module.exports = {
     'jest-watch-typeahead/filename',
     'jest-watch-typeahead/testname',
   ],
-  collectCoverage: false,
-  collectCoverageFrom: ['packages/**/src/**/*.{ts,tsx}', '!**/node_modules/**'],
 };
