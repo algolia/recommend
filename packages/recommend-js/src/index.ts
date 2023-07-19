a7e410bb69374d1c75ee27adc377d9a1eaf8dfa8
Ana Maria Gabriela Lungu <heymarialungu@gmail.com>
2023-07-19 10:14:57 +0200

    feat: add looking similar for recommend js

--- packages/recommend-js/src/index.ts
+++ packages/recommend-js/src/index.ts
@@ -1,5 +1,6 @@
 export * from './frequentlyBoughtTogether';
 export * from './relatedProducts';
+export * from './lookingSimilar';
 export * from './trendingFacets';
 export * from './trendingItems';
 export * from './types/index';
---
+++ packages/recommend-js/src/lookingSimilar.tsx
@@ -0,0 +1,101 @@
+/** @jsxRuntime classic */
+/** @jsx h */
+import {
+  getLookingSimilar,
+  GetLookingSimilarProps,
+  GetRecommendationsResult,
+} from '@algolia/recommend-core';
+import {
+  createLookingSimilarComponent,
+  LookingSimilarProps as LookingSimilarVDOMProps,
+} from '@algolia/recommend-vdom';
+import { html } from 'htm/preact';
+import { createElement, Fragment, h, render } from 'preact';
+import { useEffect, useState } from 'preact/hooks';
+
+import { getHTMLElement } from './getHTMLElement';
+import { EnvironmentProps, HTMLTemplate } from './types';
+import { useAlgoliaAgent } from './useAlgoliaAgent';
+import { useStatus } from './useStatus';
+import { withHtml } from './utils';
+
+const UncontrolledLookingSimilar = createLookingSimilarComponent({
+  createElement,
+  Fragment,
+});
+
+function useLookingSimilar<TObject>(props: GetLookingSimilarProps<TObject>) {
+  const [result, setResult] = useState<GetRecommendationsResult<TObject>>({
+    recommendations: [],
+  });
+  const { status, setStatus } = useStatus('loading');
+
+  useAlgoliaAgent({ recommendClient: props.recommendClient });
+
+  useEffect(() => {
+    setStatus('loading');
+    getLookingSimilar(props).then((response) => {
+      setResult(response);
+      setStatus('idle');
+    });
+  }, [props, setStatus]);
+
+  return {
+    ...result,
+    status,
+  };
+}
+
+type LookingSimilarProps<
+  TObject,
+  TComponentProps extends Record<string, unknown> = {}
+> = GetLookingSimilarProps<TObject> &
+  Omit<LookingSimilarVDOMProps<TObject, TComponentProps>, 'items' | 'status'>;
+
+function LookingSimilar<
+  TObject,
+  TComponentProps extends Record<string, unknown> = {}
+>(props: LookingSimilarProps<TObject, TComponentProps>) {
+  const { recommendations, status } = useLookingSimilar<TObject>(props);
+
+  return (
+    <UncontrolledLookingSimilar
+      {...props}
+      items={recommendations}
+      status={status}
+    />
+  );
+}
+
+export function lookingSimilar<TObject>({
+  container,
+  environment = window,
+  itemComponent,
+  fallbackComponent,
+  headerComponent,
+  view,
+  children,
+  ...props
+}: LookingSimilarProps<TObject, HTMLTemplate> & EnvironmentProps) {
+  const vnode = (
+    <LookingSimilar<TObject, HTMLTemplate>
+      {...props}
+      view={view && withHtml(view)}
+      itemComponent={itemComponent && withHtml(itemComponent)}
+      headerComponent={headerComponent && withHtml(headerComponent)}
+      fallbackComponent={fallbackComponent && withHtml(fallbackComponent)}
+    >
+      {children
+        ? (childrenProps) => children({ ...childrenProps, html })
+        : undefined}
+    </LookingSimilar>
+  );
+
+  if (!container) {
+    return vnode;
+  }
+
+  render(vnode, getHTMLElement(container, environment));
+
+  return null;
+}
