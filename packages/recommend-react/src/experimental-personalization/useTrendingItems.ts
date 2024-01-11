f6566f642c9fcc41c7fd3b9d033c98a37296ff78
Raed <raed.chammam@algolia.com>
2024-01-11 16:50:28 +0100

    rename hook

--- packages/recommend-react/src/experimental-personalization/useTrendingItems.ts
+++ packages/recommend-react/src/experimental-personalization/useTrendingItems.ts
@@ -1,12 +1,12 @@
 import { UseTrendingItemsProps } from '../TrendingItems';
-import { useTrendingItems as hook } from '../useTrendingItems';
+import { useTrendingItems as useHook } from '../useTrendingItems';
 
 import { Personalization } from './types';
 
 type Props<TObject> = UseTrendingItemsProps<TObject> & Personalization;
 
 export function useTrendingItems<TObject>(props: Props<TObject>) {
-  const { recommendations, status } = hook(props);
+  const { recommendations, status } = useHook(props);
   console.log('hook call', status);
   return { recommendations, status };
 }
