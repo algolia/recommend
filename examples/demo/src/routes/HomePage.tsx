c75dcf53491c4c3221e2ffdf789abada5bb75868
Raed <raed.chammam@algolia.com>
2024-01-31 12:30:16 +0100

    update react demo

--- examples/demo/src/routes/HomePage.tsx
+++ examples/demo/src/routes/HomePage.tsx
@@ -24,12 +24,9 @@ export const HomePage: React.FC = () => {
   ] = useApplicationContext();
 
   return (
-    <Recommend
-      recommendClient={recommendClient}
-      userToken="likes-gender-men"
-      region="eu"
-    >
+    <>
       <TrendingFacets
+        recommendClient={recommendClient}
         indexName={indexName}
         facetName="brand"
         itemComponent={({ item }) => (
@@ -51,11 +48,15 @@ export const HomePage: React.FC = () => {
         }}
       />
       <TrendingItems<ProductHit>
+        recommendClient={recommendClient}
         indexName={indexName}
         facetName={selectedFacetValue ? 'brand' : undefined}
         facetValue={
           selectedFacetValue ? selectedFacetValue.facetValue : undefined
         }
+        userToken="likes-gender-men"
+        region="eu"
+        suppressExperimentalWarning={true}
         itemComponent={({ item }) => (
           <Hit
             hit={item}
@@ -75,6 +76,7 @@ export const HomePage: React.FC = () => {
         }}
       />
       <RecommendedForYou<ProductHit>
+        recommendClient={recommendClient}
         indexName={indexName}
         maxRecommendations={15}
         queryParameters={{
@@ -92,6 +94,6 @@ export const HomePage: React.FC = () => {
         )}
         fallbackComponent={() => <div>No recommendations</div>}
       />
-    </Recommend>
+    </>
   );
 };
