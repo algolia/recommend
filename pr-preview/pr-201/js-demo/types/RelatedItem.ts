a8200546604d32b62fe4c5f02029844b3ac9b423
raed667 <raed667@users.noreply.github.com>
2026-02-25 15:22:42 +0000

    Remove preview for PR 201 🛬

--- pr-preview/pr-201/js-demo/types/RelatedItem.ts
+++
@@ -1,9 +0,0 @@
-import { InsightsClient } from 'search-insights';
-
-import { ProductHit } from './ProductHit';
-
-export type RelatedItemProps = {
-  item: ProductHit;
-  insights: InsightsClient;
-  onSelect(hit: ProductHit): void;
-};
