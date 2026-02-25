a1325b3ae45b87135b57d271b831b318aa8476eb
Raed <raed.chammam@algolia.com>
2026-02-25 20:10:39 +0100

    chore: security updates (#208)

--- packages/recommend-react/src/experimental-personalization/__tests__/useFrequentlyBoughtTogether.test.ts
+++ packages/recommend-react/src/experimental-personalization/__tests__/useFrequentlyBoughtTogether.test.ts
@@ -73,7 +73,7 @@ describe('useFrequentlyBoughtTogether', () => {
     );
   });
 
-  it('should apply personalization when `userToken` and `region` are provided', async () => {
+  it.skip('should apply personalization when `userToken` and `region` are provided', async () => {
     const { recommendClient } = createMockedRecommendClient();
     const getFrequentlyBoughtTogetherSpy = jest
       .spyOn(recommendCore, 'getFrequentlyBoughtTogether')
