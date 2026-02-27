af9f78375568ac8eeb89d45a3ca67ee66a2d0c39
raed667 <raed667@users.noreply.github.com>
2026-02-27 10:07:24 +0000

    Deploy preview for PR 212 🛫

---
+++ pr-preview/pr-212/templating/types/ProductHit.ts
@@ -0,0 +1,34 @@
+import { Hit } from '@algolia/client-search';
+
+export type ProductRecord = {
+  brand: string;
+  image_urls: string[];
+  hierarchical_categories: {
+    lvl0: string;
+    lvl1?: string;
+    lvl2?: string;
+    lvl3?: string;
+    lvl4?: string;
+    lvl5?: string;
+  };
+  name: string;
+  price: {
+    value: number;
+    currency: string;
+    discount_level: number;
+    discounted_value: number;
+    on_sales: boolean;
+  };
+  _score: number;
+  reviewScore: number;
+  reviewCount: number;
+  url: string;
+};
+
+type WithInsights<THit> = THit & {
+  __position: string;
+  __indexName: string;
+  __queryID: string;
+};
+
+export type ProductHit = WithInsights<Hit<ProductRecord>>;
