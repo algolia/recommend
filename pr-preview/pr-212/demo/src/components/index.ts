79924193990e75e7f18a9f8eecd5a3ae2a9ea3b9
raed667 <raed667@users.noreply.github.com>
2026-02-27 10:12:17 +0000

    Remove preview for PR 212 🛬

--- pr-preview/pr-212/demo/src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './Autocomplete';
-export * from './BundleItem';
-export * from './BundleView';
-export * from './Facet';
-export * from './Hit';
-export * from './ComparisonChartView';
